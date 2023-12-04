72bf96cf9015a621225139a4279ffb3280fd05d0
hjr-bill <1326112622@qq.com>
2023-12-04 12:26:58 +0800

    feat: Optimize mqtt packaging

--- pkg/mqtt/service.go
+++ pkg/mqtt/service.go
@@ -5,8 +5,8 @@ import (
 	iolog "ims-server/pkg/logger"
 )
 
-func Sub(client mqtt.Client, topic string, handler mqtt.MessageHandler) error {
-	if token := client.Subscribe(topic, 1, handler); token.Wait() && token.Error() != nil {
+func (mc *mqttClient) Sub(topic string, handler mqtt.MessageHandler) error {
+	if token := mc.client.Subscribe(topic, 1, handler); token.Wait() && token.Error() != nil {
 		iolog.Error("mqtt subscribe failed, err: %v", token.Error())
 		return token.Error()
 	}
@@ -14,33 +14,33 @@ func Sub(client mqtt.Client, topic string, handler mqtt.MessageHandler) error {
 	return nil
 }
 
-func Unsub(client mqtt.Client, topic string) {
-	if token := client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
+func (mc *mqttClient) Unsub(topic string) {
+	if token := mc.client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
 		iolog.Error("mqtt unsubscribe failed, err: %v", token.Error())
 	}
 }
 
-func SetRetained(client mqtt.Client, topic string, text string) {
-	token := client.Publish(topic, 0, true, text)
+func (mc *mqttClient) SetRetained(topic string, text string) {
+	token := mc.client.Publish(topic, 0, true, text)
 	token.Wait()
 }
 
-func Publish(client mqtt.Client, topic string, text string, qos ...byte) {
+func (mc *mqttClient) Publish(topic string, text string, qos ...byte) {
 	quality := byte(0)
 	if qos != nil {
 		quality = qos[0]
 	}
-	token := client.Publish(topic, quality, false, text)
+	token := mc.client.Publish(topic, quality, false, text)
 	token.Wait()
 }
 
-func MPublish(client mqtt.Client, topic string, texts []string, qos ...byte) {
+func (mc *mqttClient) MPublish(topic string, texts []string, qos ...byte) {
 	quality := byte(0)
 	if qos != nil {
 		quality = qos[0]
 	}
 	for _, t := range texts {
-		token := client.Publish(topic, quality, false, t)
+		token := mc.client.Publish(topic, quality, false, t)
 		token.Wait()
 	}
 }
