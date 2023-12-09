ccf502bed6676fef8b5959583459dcce757a6cda
hjr-bill <1326112622@qq.com>
2023-12-10 00:54:39 +0800

    fix: Fix bug using gorm's size tag

--- backend/microservices/device/internal/dal/model/data.go
+++ backend/microservices/device/internal/dal/model/data.go
@@ -7,11 +7,11 @@ import (
 
 type Data struct {
 	gorm.Model
-	Type       job.SensorType `gorm:"type:string;size:15;comment:传感器类型;not null"`
+	Type       job.SensorType `gorm:"type:char(15);comment:传感器类型;not null"`
 	TerminalID string         `gorm:"type:string;comment:终端 ID;not null"`
 	SensorData interface{}    `gorm:"type:string;comment:传感器数据 not null"`
 }
 
 func (Data) TableName() string {
-	return "ims_mqtt_SensorDate"
+	return "ims_mqtt_date"
 }
--- backend/microservices/user/internal/dal/model/user.go
+++ backend/microservices/user/internal/dal/model/user.go
@@ -8,16 +8,16 @@ import (
 // User 用户
 type User struct {
 	gorm.Model
-	Type ioconsts.UserType `gorm:"type:char;size:15;comment:账户类型;not null"`
+	Type ioconsts.UserType `gorm:"type:char(15);comment:账户类型;not null"`
 
-	Name     string `gorm:"type:char;size:10;comment:用户名;unique"`
+	Name     string `gorm:"type:char(10);comment:用户名;unique"`
 	Password string `gorm:"type:string;comment:密码;not null"`
-	Email    string `gorm:"type:char;size:30;comment:邮箱;unique"`
+	Email    string `gorm:"type:char(30);comment:邮箱;unique"`
 
-	PhoneNumber string `gorm:"type:char;size:20;comment:手机号"`
+	PhoneNumber string `gorm:"type:char(20);comment:手机号"`
 	Avatar      string `gorm:"type:string;comment:头像"`
 
-	Status ioconsts.AccountStatus `gorm:"type:char;size:10;comment:用户状态"`
+	Status ioconsts.AccountStatus `gorm:"type:char(10);comment:用户状态"`
 }
 
 func (User) TableName() string {
